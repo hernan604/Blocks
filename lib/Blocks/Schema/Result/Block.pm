87b779ef3c76752c0d4be5af4ef5b80c338ced19
Frederico Recsky <devel@imovlr.com>
2014-12-01 09:09:55 -0800

    Add block result set

---
+++ lib/Blocks/Schema/Result/Block.pm
@@ -0,0 +1,108 @@
+use utf8;
+package Blocks::Schema::Result::Block;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+Blocks::Schema::Result::Block
+
+=cut
+
+use strict;
+use warnings;
+
+use Moose;
+use MooseX::NonMoose;
+use MooseX::MarkAsMethods autoclean => 1;
+extends 'DBIx::Class::Core';
+
+=head1 COMPONENTS LOADED
+
+=over 4
+
+=item * L<DBIx::Class::InflateColumn::DateTime>
+
+=back
+
+=cut
+
+__PACKAGE__->load_components("InflateColumn::DateTime");
+
+=head1 TABLE: C<Blocks>
+
+=cut
+
+__PACKAGE__->table("Blocks");
+
+=head1 ACCESSORS
+
+=head2 idblock
+
+  data_type: 'integer'
+  extra: {unsigned => 1}
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 title
+
+  data_type: 'varchar'
+  is_nullable: 1
+  size: 255
+
+=head2 timestamp
+
+  data_type: 'timestamp'
+  datetime_undef_if_invalid: 1
+  default_value: current_timestamp
+  is_nullable: 0
+
+=head2 content
+
+  data_type: 'text'
+  is_nullable: 1
+
+=cut
+
+__PACKAGE__->add_columns(
+  "idblock",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_auto_increment => 1,
+    is_nullable => 0,
+  },
+  "title",
+  { data_type => "varchar", is_nullable => 1, size => 255 },
+  "timestamp",
+  {
+    data_type => "timestamp",
+    datetime_undef_if_invalid => 1,
+    default_value => \"current_timestamp",
+    is_nullable => 0,
+  },
+  "content",
+  { data_type => "text", is_nullable => 1 },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</idblock>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("idblock");
+
+
+# Created by DBIx::Class::Schema::Loader v0.07038 @ 2014-11-28 04:58:33
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:hTjnSMTOVx/Nq5J3epanZQ
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+__PACKAGE__->meta->make_immutable;
+1;
