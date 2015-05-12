88fe48525be817fcea8e0edceda51fc507a20d63
fredericorecsky <cartas@frederico.me>
2015-05-12 05:59:47 -0300

    add config template exploder

---
+++ bin/configutil.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Data::Dumper;
+
+use Catalyst qw/ConfigLoader/;
+use Blocks;
+use Template;
+
+if ( -e $ARGV[0] ){
+
+    my $filename = $ARGV[0];
+    my $block = Blocks->config();
+    my $template = Template->new();
+    my $output;
+
+    print Dumper $block;
+
+    $template->process( $filename, { config =>  $block }, $filename . ".output" )
+        or die "Error when processing the template $filename ". $template->error();
+} else {
+    die "Need to receive a valid file as argument";
+}
+
+
+#print Dumper $block->{ 'Model::Blocks' }{ 'connect_info' };
