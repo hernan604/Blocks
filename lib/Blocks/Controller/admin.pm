bfc91fa8c37fc7cae4a48c8c8529bbbe1f8e76b2
Frederico Recsky <cartas@frederico.me>
2015-05-19 01:54:42 -0300

    Add Languages basic support

--- lib/Blocks/Controller/admin.pm
+++ lib/Blocks/Controller/admin.pm
@@ -2,6 +2,8 @@ package Blocks::Controller::admin;
 use Moose;
 use namespace::autoclean;
 
+use Data::Dumper;
+
 BEGIN { extends 'Catalyst::Controller'; }
 
 =head1 NAME
@@ -20,6 +22,12 @@ Catalyst Controller.
 =head2 index
 
 =cut
+
+sub begin :Private {
+    my ( $self, $c ) = @_;
+
+    $c->session->{ admin } = "admin";
+}
 
 # login
 sub index :Path :Args(0) {
@@ -40,13 +48,15 @@ sub block_GET :Args(1){
         $block = $block_rs->find($arg);
     }
 
+    print Dumper $c->session;
+
     if ( $block ){
         $c->stash(
             block => $block,
         );
     }else{
         $c->stash(
-            
+
         );
     }
 }
@@ -61,19 +71,63 @@ sub block_POST :Args(1) {
     my $block;
     if ( $arg ){
         $block = $block_rs->find($arg);
-        $block->title( $title );
-        $block->content( $content );
-        $block->update();
+        if ( $block ) {
+            $block->title( $title );
+            $block->content( $content );
+            $block->update();
+        }else{
+            $c->response->code( 500 );
+        }
     }else{
-        $block->create({
+        $block = $block_rs->create({
             title => $title,
             content => $content,
         });
+        $block->update();
     }
     $c->response->redirect( '/admin/block/'. $block->idblock() );
 }
 
+sub languages :Local :ActionClass( 'REST' ) { }
 
+sub languages_GET :Args(1) {
+    my ( $self, $c, $arg ) = @_;
+
+    my $languages_rs = $c->model( 'Blocks::Language' );
+    my @languages = $languages_rs->search( { idblocks => $arg } );
+
+    $c->stash({
+        idblock     => $arg,
+        languages   => \@languages,
+    });
+
+}
+
+sub languages_POST :Args(1) {
+    my ( $self, $c, $arg ) = @_;
+
+    my $language = $c->request->param("language");
+
+    my $block_rs = $c->model( 'Blocks::Block' );
+    my $languages_rs = $c->model( 'Blocks::Language' );
+
+    my ( $block, $lang );
+
+    $block = $block_rs->create({
+        title => undef,
+        content => undef,
+    });
+    $block->update();
+
+    $lang = $languages_rs->create({
+        idblocks        => $arg,
+        language        => $language,
+        linked_block    => $block->idblock(),
+    });
+
+    $c->response->redirect( '/admin/block/' .  $block->idblock() );
+
+}
 
 =encoding utf8
 
